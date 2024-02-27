9be62552ca3846ce56ac59394e66cb2aa8ca1965
zhangzheyong <zhangzheyong@turingq.com>
2024-02-27 23:18:50 +0800

    catppuin

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
+  -- colorscheme = "astrodark",
   colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
