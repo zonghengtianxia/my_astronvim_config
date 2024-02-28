2e61d1f809a7d7bf9ad34852dffe203e4defa621
zhangzheyong <zhangzheyong@turingq.com>
2024-02-28 15:43:54 +0800

    mini-animate

--- plugins/community.lua
+++ plugins/community.lua
@@ -25,6 +25,40 @@ return {
     },
   },
   { import = "astrocommunity.scrolling.mini-animate" },
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function()
+      -- don't use animate when scrolling with the mouse
+      local mouse_scrolled = false
+      for _, scroll in ipairs { "Up", "Down" } do
+        local key = "<ScrollWheel" .. scroll .. ">"
+        vim.keymap.set({ "", "i" }, key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
+
+      local animate = require "mini.animate"
+      return {
+        resize = {
+          timing = animate.gen_timing.linear { duration = 100, unit = "total" },
+        },
+        scroll = {
+          timing = animate.gen_timing.linear { duration = 150, unit = "total" },
+          subscroll = animate.gen_subscroll.equal {
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          },
+        },
+      }
+    end,
+  },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.code-runner.sniprun" },
   { import = "astrocommunity.completion.tabnine-nvim" },
